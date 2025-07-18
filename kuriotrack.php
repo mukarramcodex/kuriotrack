9542a92f0e384983cdd7c95940ead7adc4086bde
Mukarram Ali <info.mukarramali@gmail.com>
2025-07-18 18:26:31 +0500

    inital commit

---
+++ kuriotrack.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Kurio Track
+ * Plugin URI: https://kuriotrack.com
+ * Description: Short description of what your plugin does.
+ * Version: 1.0.1
+ * Author: Mukarram Solutions
+ */
+
+defined('ABSPATH') || exit;
